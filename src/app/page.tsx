fed55ee6575789d33cb08d08cfe792844f1f392a
KruyTharin <kruytharin17@gmail.com>
2024-06-30 04:09:29 +0700

    feat: prisma setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@/components/ui/button';
-import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
   return (
     <div>
       <Button>Click me</Button>
